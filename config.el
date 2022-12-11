4eb93bed3ec89aebc2363df08b6878d598409d6d
darkhandz <darkhandz0@gmail.com>
2022-12-11 22:03:27 +0800

    add citre

--- config.el
+++ config.el
@@ -231,3 +231,45 @@
   (modify-syntax-entry ?_ "w"))
 
 ;; -----------------------------------------------------------------------------------------
+(use-package citre
+  :defer t
+  :init
+  ;; This is needed in `:init' block for lazy load to work.
+  (require 'citre-config)
+  ;; Bind your frequently used commands.  Alternatively, you can define them
+  ;; in `citre-mode-map' so you can only use them when `citre-mode' is enabled.
+  (global-set-key (kbd "C-c j") 'citre-jump)
+  (global-set-key (kbd "C-c h") 'citre-jump-back)
+  (global-set-key (kbd "<f1>") 'citre-peek)
+  (global-set-key (kbd "C-c c") 'citre-peek)
+  (global-set-key (kbd "C-c a") 'citre-ace-peek)
+  (global-set-key (kbd "C-c u") 'citre-update-this-tags-file)
+  :config
+  (setq citre-edit-cmd-buf-default-cmd "ctags\n-o\n%TAGSFILE%\n;; programming languages to be scanned, or \"all\" for all supported languages\n--languages=all\n--kinds-all=*\n--fields=*\n--extras=*\n--exclude=**/.ccls-cache/*\n-R\n;; add dirs/files to scan here, one line per dir/file\n")
+  (setq
+   ;; Set these if readtags/ctags is not in your PATH.
+   ;; citre-readtags-program "/path/to/readtags"
+   ;; citre-ctags-program "/path/to/ctags"
+   ;; Set these if gtags/global is not in your PATH (and you want to use the
+   ;; global backend)
+   ;; citre-gtags-program "/path/to/gtags"
+   ;; citre-global-program "/path/to/global"
+   ;; Set this if you use project management plugin like projectile.  It's
+   ;; used for things like displaying paths relatively, see its docstring.
+   citre-project-root-function #'projectile-project-root
+   ;; Set this if you want to always use one location to create a tags file.
+   ;; citre-default-create-tags-file-location 'global-cache
+   ;; See the "Create tags file" section above to know these options
+   citre-use-project-root-when-creating-tags t
+   citre-prompt-language-for-ctags-command t
+   ;; By default, when you open any file, and a tags file can be found for it,
+   ;; `citre-mode' is automatically enabled.  If you only want this to work for
+   ;; certain modes (like `prog-mode'), set it like this.
+   citre-auto-enable-citre-mode-modes '(prog-mode)))
+
+
+
+
+
+
+;; -----------------------------------------------------------------------------------------
