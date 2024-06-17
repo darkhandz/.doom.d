bc787c9e951b76c2a957a6d07d77a09d5b4f06d8
darkhandz <darkhandz0@gmail.com>
2024-06-17 19:46:02 +0800

    add a func to reload dir-locals

--- config.el
+++ config.el
@@ -509,3 +509,27 @@
   (map! :map vterm-mode-map :ni "M-7" #'+workspace/switch-to-6)
   (map! :map vterm-mode-map :ni "M-8" #'+workspace/switch-to-7)
   (map! :map vterm-mode-map :ni "M-9" #'+workspace/switch-to-8))
+
+
+(defun project-reload-dir-locals (proj)
+  "Read values from the current project's .dir-locals file and
+apply them in all project file buffers as if opening those files
+for the first time.
+
+Signals an error if there is no current project."
+  (interactive (list (project-current)))
+  (unless proj
+    (user-error "There doesn't seem to be a project here"))
+  ;; Load the variables; they are stored buffer-locally, so...
+  (hack-dir-local-variables)
+  (message "test")
+  ;; Hold onto them...
+  (let ((locals dir-local-variables-alist))
+    (dolist (buffer (buffer-list))
+      (with-current-buffer buffer
+        (when (and (equal proj (project-current))
+                   buffer-file-name)
+          ;; transfer the loaded values to this buffer...
+          (setq-local dir-local-variables-alist locals)
+          ;; and apply them.
+          (hack-local-variables-apply))))))
